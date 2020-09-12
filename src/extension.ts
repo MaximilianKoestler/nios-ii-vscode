fe45354d25d106365e895c1adeb0a3c43afe239e
Maximilian Köstler <maximilian@koestler.hamburg>
2020-09-12 17:59:43 +0200

    Add hover text for assembly instructions

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,14 @@
 import * as vscode from "vscode";
 import { hoverText } from "./hovertext";
+import * as fs from "fs";
+import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
+  const reference_path = context.asAbsolutePath(
+    path.join("resources", "reference.json")
+  );
+  const reference_data = JSON.parse(fs.readFileSync(reference_path, "utf8"));
+
   vscode.languages.registerHoverProvider("niosii", {
     provideHover(document, position, token) {
       const line = document.lineAt(position.line);
@@ -15,7 +22,7 @@ export function activate(context: vscode.ExtensionContext) {
       const range = document.getWordRangeAtPosition(position);
       const word = document.getText(range);
 
-      const text = hoverText(word);
+      const text = hoverText(reference_data, word);
       return text === null
         ? null
         : {
--- src/hovertext.ts
+++ src/hovertext.ts
@@ -1,3 +1,76 @@
-export function hoverText(word: string): string | null {
-  return word;
+interface ReferenceEntry {
+  type: "instruction" | "register" | "alias";
+}
+
+interface InstructionEntry extends ReferenceEntry {
+  mnemonic: string;
+  instruction: string;
+  operation: string;
+  assemblerSyntax: string;
+  example: string;
+  description: string;
+  usage?: string;
+}
+
+interface AliasEntry extends ReferenceEntry {
+  alias: string;
+}
+
+type ReferenceData = { [mnemonic: string]: ReferenceEntry };
+
+function uppercaseFirst(text: string): string {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+function formatInstruction(instruction: InstructionEntry): string {
+  let text = `
+**${uppercaseFirst(instruction.instruction)}**
+
+**Operation:** ${instruction.operation}
+
+**Syntax:**
+\`\`\`niosii
+${instruction.assemblerSyntax}
+\`\`\`
+
+**Example:**
+\`\`\`niosii
+${instruction.example}
+\`\`\`
+
+**Description:**
+
+${uppercaseFirst(instruction.description)}
+  `;
+
+  if (instruction.usage !== undefined) {
+    text += `
+**Usage:**
+
+${uppercaseFirst(instruction.usage)}
+`;
+  }
+
+  return text;
+}
+
+export function hoverText(
+  referenceData: ReferenceData,
+  word: string
+): string | null {
+  if (!(word in referenceData)) {
+    return null;
+  }
+
+  const reference = referenceData[word];
+
+  switch (reference.type) {
+    case "instruction":
+      return formatInstruction(reference as InstructionEntry);
+    case "alias":
+      return hoverText(referenceData, (reference as AliasEntry).alias);
+    default:
+      console.warn("Unsupported reference type:", reference.type);
+      return null;
+  }
 }
